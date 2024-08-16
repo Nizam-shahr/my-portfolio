5d9746c3fffb60167482b97f6745eab8dd62bab0
Nizam-shahr <nizamushahru@gmail.com>
2024-08-16 15:07:59 +0100

    jjj

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
 import Layout from "@/components/Layout";
 import Sidebar from "@/components/Sidebar";
 import About from "@/components/About";
